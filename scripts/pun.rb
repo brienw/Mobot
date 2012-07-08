d4b1b569f5076aea62085b8244cacc27513a1bb3
Pablo Merino <pablo.perso1995@gmail.com>
2012-07-08 17:24:28 +0200

    yay brobot!

--- scripts/pun.rb
+++ scripts/pun.rb
@@ -3,12 +3,19 @@
 #	Example: brobot github Studio182 Brobot
 #
 
+require 'feedzirra'
 
 class Pun
 
 	def command(params, nick)
-		puns = ["I wondered why the baseball was getting bigger. Then it hit me.", "I'm reading a book about anti-gravity. It's impossible to put down.", "Did you hear about the guy whose whole left side was cut off? He's all right now.", "It's not that the man did not know how to juggle, he just didn't have the balls to do it.", "I couldn't quite remember how to throw a boomerang, but eventually it came back to me.", "There was a sign on the lawn at a drug re-hab center that said 'Keep off the Grass'.", "I used to have a fear of hurdles, but I got over it.", "I was going to look for my missing watch, but I could never find the time.", "A hole has been found in the nudist camp wall. The police are looking into it.", "He drove his expensive car into a tree and found out how the Mercedes bends.", "Don't trust people that do acupuncture, they're back stabbers.", "A bicycle can't stand on its own because it is two-tired.", "A small boy swallowed some coins and was taken to a hospital. When his grandmother telephoned to ask how he was a nurse said 'No change yet'.", "When William joined the army he disliked the phrase 'fire at will'.", "I did a theatrical performance about puns. Really it was just a play on words.", "Show me a piano falling down a mineshaft and I'll show you A-flat minor.", "I recently bought 51% of a vampire hunting company. I'm now the main stake holder.", "Alcohol and calculus don't mix so don't drink and derive."]
-		puns.sample
+
+    feed = Feedzirra::Feed.fetch_and_parse("http://feeds.feedburner.com/PunOfTheDay")
+    entry = feed.entries.first
+    entry.summary.gsub(/(<[^>]*>)|\n|\t/s, "").gsub("[Click to Vote!]", "")
+
+
+		#puns = ["I wondered why the baseball was getting bigger. Then it hit me.", "I'm reading a book about anti-gravity. It's impossible to put down.", "Did you hear about the guy whose whole left side was cut off? He's all right now.", "It's not that the man did not know how to juggle, he just didn't have the balls to do it.", "I couldn't quite remember how to throw a boomerang, but eventually it came back to me.", "There was a sign on the lawn at a drug re-hab center that said 'Keep off the Grass'.", "I used to have a fear of hurdles, but I got over it.", "I was going to look for my missing watch, but I could never find the time.", "A hole has been found in the nudist camp wall. The police are looking into it.", "He drove his expensive car into a tree and found out how the Mercedes bends.", "Don't trust people that do acupuncture, they're back stabbers.", "A bicycle can't stand on its own because it is two-tired.", "A small boy swallowed some coins and was taken to a hospital. When his grandmother telephoned to ask how he was a nurse said 'No change yet'.", "When William joined the army he disliked the phrase 'fire at will'.", "I did a theatrical performance about puns. Really it was just a play on words.", "Show me a piano falling down a mineshaft and I'll show you A-flat minor.", "I recently bought 51% of a vampire hunting company. I'm now the main stake holder.", "Alcohol and calculus don't mix so don't drink and derive."]
+		#puns.sample
 
 	end
 end
