fd2d833bae21a653990a1b630909490be9e00e8b
Sebastian Jachec <sjachec1@gmail.com>
2012-07-09 17:31:16 +0100

    Minor Google tweak

--- scripts/google.rb
+++ scripts/google.rb
@@ -7,8 +7,16 @@ module BrobotScript
 	class Google
 		require 'uri'
 		def command(params, nick)
+			connectingWords = %w(for me please)
+
+			connectingWords.each do |word|
+				params.delete word
+			end
+
+			starters = ["Sure. Here it is - ", "Here - ", "Here you go #{nick}" ,""]
+
 			params = params.join(" ")
-			"http://www.google.com/search?sourceid=chrome&ie=UTF-8&q=#{params}"
+			"#{starters.sample}http://www.google.com/search?sourceid=chrome&ie=UTF-8&q=#{params}"
 		end
 	end
 end
