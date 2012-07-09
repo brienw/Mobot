e6cfde727a219d7ac3fec83a382aa0dc9e1f12ea
Sebastian Jachec <sjachec1@gmail.com>
2012-07-09 17:10:00 +0100

    Why thank you...

---
+++ scripts/thanks.rb
@@ -0,0 +1,14 @@
+#
+#	Checks if the bot works
+#	Example: brobot ping
+#
+
+module BrobotScript
+	class Ping
+
+		def command(params, nick)
+			replies = ["No problem", "No problem #{nick}", "It's what I'm here for \u{1f60a}"]
+			replies.sample
+		end
+	end
+end
